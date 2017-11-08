d0c1aa758a3a6c415d7a5c5dafb3f8860d588891
Ana <you@example.com>
2017-11-08 01:00:50 -0200

    zoom funcionando

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $("#btn1").click(function(evt) {
         $("#zoome").zoomTo({targetsize:0.75, duration:600});
-        $(".relative").click(function(evt2){
-          $(".relative").zoomTo({targetsize:0.65, duration:600});
+        $(".background_class").click(function(evt2){
+          $(".background_class").zoomTo({targetsize:1, duration:600});
           evt2.stopPropagation();
         });
         evt.stopPropagation();
