d00ca2005473a868476badea9e431875f40c8c00
Ana <you@example.com>
2017-11-08 00:57:24 -0200

    gambiarra pra voltar o zoom

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
     $("#btn1").click(function(evt) {
-        $("#zoome").zoomTo({targetsize:0.75, duration:600, closeclick:true});
+        $("#zoome").zoomTo({targetsize:0.75, duration:600});
+        $(".relative").click(function(evt2){
+          $(".relative").zoomTo({targetsize:0.65, duration:600});
+          evt2.stopPropagation();
+        });
         evt.stopPropagation();
     });
 });
