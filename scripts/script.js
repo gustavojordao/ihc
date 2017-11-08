e989568bafff2f4fc68f454a6c5b1ebe33aa7eff
Bernardo Meneghini <bmuschioni@gmail.com>
2017-11-08 06:12:28 -0200

    Alterações CSS e JS

--- scripts/script.js
+++ scripts/script.js
@@ -7,4 +7,53 @@ $(document).ready(function() {
         });
         evt.stopPropagation();
     });
+
+	function errou(e){
+		var element = e.target;
+		if(element.tagName == 'SPAN')
+			element = element.parentElement;
+
+		element.classList.add('errado');
+
+		document.getElementById("correct_answer").innerHTML = "<span class=\"text_question\">7</span>";
+
+		document.getElementById("frase_mensagem").innerHTML = "Que pena! Você errou.";
+		document.getElementById("menu").classList.remove("menu_oculto");
+		document.getElementById("menu").classList.add("menu_exibido");
+
+		document.getElementById("item01").removeEventListener('click', e, false);
+		document.getElementById("item02").removeEventListener('click', e, false);
+		document.getElementById("item03").removeEventListener('click', e, false);
+	}
+
+	function acertou(e){
+		var element = e.target;
+		if(element.tagName == 'SPAN')
+			element = element.parentElement;
+
+		element.classList.add('certo');
+
+		document.getElementById("correct_answer").innerHTML = "<span class=\"text_question\">7</span>";
+
+		document.getElementById("frase_mensagem").innerHTML = "Parabéns! Você acertou.";
+		document.getElementById("menu").classList.remove("menu_oculto");
+		document.getElementById("menu").classList.add("menu_exibido");
+
+		document.getElementById("item01").removeEventListener('click', e, false);
+		document.getElementById("item02").removeEventListener('click', e, false);
+		document.getElementById("item03").removeEventListener('click', e, false);
+	}
+
+	document.getElementById("item01").addEventListener('click', function(e){
+		errou(e);
+	});
+
+	document.getElementById("item02").addEventListener('click', function(e){
+		errou(e);
+	});
+
+	document.getElementById("item03").addEventListener('click', function(e){
+		acertou(e);
+	});
+
 });
